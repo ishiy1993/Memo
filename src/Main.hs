702297867ddbfa217dbc9696258a0b87e8bb5f97
iyou <y.ishihara.1993@gmail.com>
2015-07-28 09:48:31 +0900

    Add appendFile

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,9 @@
 import Control.Applicative
 
 main = do contents <- lines <$> getContents
-          putStrLn . unlines $ toMd contents
+          appendFile "/home/iyou/memos/test.md" . unlines $ toMd contents
 
 toMd :: [String] -> [String]
 toMd (x:xs) = y:ys
               where y = "# " ++ x
-                    ys = map ("\t+ " ++) xs
+                    ys = map ("  + " ++) xs
