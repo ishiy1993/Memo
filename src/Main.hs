ca27ce0dd5b14c041f1c42679556e99623dfe218
iyou <y.ishihara.1993@gmail.com>
2015-07-28 15:48:00 +0900

    Add daytime, tags, contents-number

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,28 @@
 import Control.Applicative
+import System.Environment
+import Data.Time
+import Data.Time.Format
+import Data.Time.Clock
+import Data.Time.LocalTime
 
-main = do contents <- lines <$> getContents
-          appendFile "/home/iyou/memos/test.md" . unlines $ toMd contents
+main = do contents <- filter (/= "") . lines <$> getContents
+          args <- getArgs
+          tz <- getCurrentTimeZone
+          utc <- getCurrentTime
+          let dy = daytime tz utc
+              tag = toTag args
+          appendFile "/home/iyou/memos/test.md" . unlines $ toMd dy tag contents
 
-toMd :: [String] -> [String]
-toMd (x:xs) = y:ys
-              where y = "# " ++ x
-                    ys = map ("  + " ++) xs
+toMd :: String -> String -> [String] -> [String]
+toMd dy tag (x:xs) = y:ys
+                      where y = "# [" ++ dy ++ "] " ++ x ++ " " ++ tag ++ num
+                            ys = map ("  + " ++) xs
+                            num = " (" ++ (show $ length xs) ++ ")"
+
+daytime :: TimeZone -> UTCTime -> String
+daytime tz utc = formatTime defaultTimeLocale "%Y%m%d%H%M" $ utcToLocalTime tz utc
+
+toTag :: [String] -> String
+toTag [] = ""
+toTag xs = foldr (++) ":" ys
+            where ys = map (":" ++) xs
